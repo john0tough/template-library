2c7f50bc4b64cd83b74883c962a3028205240e76
john0tough <42975148+john0tough@users.noreply.github.com>
2020-02-10 14:36:55 -0600

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  assetPrefix: process.env.BASE_PATH || '',
+  exportTrailingSlash: true,
+  exportPathMap: async function () {
+    return {
+      '/': { page: '/' },
+      '/about': { page: '/about' }
+    }
+  },
+  publicRuntimeConfig: {
+    basePath: process.env.BASE_PATH,
+  },
+}
